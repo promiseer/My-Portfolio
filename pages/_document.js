c6b1c6d671965e4f89bb09f318dae9ae1abffc9f
Parmeshwar Rathod <77802905+promiseer@users.noreply.github.com>
2023-10-02 12:51:26 +0000

    feat: added GTM

--- pages/_document.js
+++ pages/_document.js
@@ -50,7 +50,23 @@ export default class MyDocument extends Document {
           <meta name="twitter:title" content="Parmeshwar Rathod's Portfolio - Backend/Fullstack Developer"/>
           <meta name="twitter:description" content="Explore the web development and design work of John Smith, a passionate and experienced web developer. View projects, skills, and achievements in one place."/>
           <meta name="twitter:image" content="https://cdn.buymeacoffee.com/uploads/profile_pictures/2023/09/KNADODeXA0V5GgWb.jpg@300w_0e.webp"/>
-         
+          <script
+              type="text/javascript"
+              async
+              dangerouslySetInnerHTML={{
+                __html: `
+              (function(w, d, s, l, i) {
+                    w[l] = w[l] || []
+                    w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
+                    const f = d.getElementsByTagName(s)[0],
+                      j = d.createElement(s),
+                      dl = l != "dataLayer" ? "&l=" + l : ""
+                    j.async = true
+                    j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl
+                    f.parentNode.insertBefore(j, f)
+                  })(window,document,'script','dataLayer',"GTM-MTLC5J2X")`,
+              }}
+            />
           <script
             key="structured-1"
             type="application/ld+json"
@@ -85,6 +101,14 @@ export default class MyDocument extends Document {
         </Head>
 
         <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-MTLC5J2X`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
           <Main />
           <NextScript />
           <div id="modal-root" />
